bc8db26e02e29f796be3f0fc6f5eab59accdef4d
Dante Buhl <dbuhl@ucsc.edu>
2024-02-05 21:15:36 -0800

    Added 213A hw3 assignment

---
+++ 213A/hw/hw3/fortran/LinAl.f90
@@ -0,0 +1,50 @@
+module LinAl
+  implicit none
+  
+  integer, save :: msize, nsize
+  real, dimension(:,:), allocatable, save :: mat
+  
+
+contains
+
+  !********************************************************
+
+  subroutine readMat(filename)
+
+    implicit none
+    character(len=*) :: filename
+
+    integer :: i,j
+
+    ! Reads a file containing the matrix A 
+    ! Sample file:
+    !
+    ! 4 4 
+    ! 2.0 1.0 1.0 0.0
+    ! 4.0 3.0 3.0 1.0
+    ! 8.0 7.0 9.0 5.0
+    ! 6.0 7.0 9.0 8.0
+    !
+    ! Note that the first 2 numbers in the first line are the matrix dimensions, i.e., 4x4,
+    ! then the next msize lines are the matrix entries. This matrix is found in Eq. 2.18 of the lecture note.
+    ! Note that entries must be separated by a tab.
+
+
+    open(10,file=filename)
+
+    ! Read the matrix dimensions
+    read(10,*) i,j
+
+    ! Read matrix
+    do i=1,msize
+       read(10,*) ( mat(i,j), j=1,nsize )
+    enddo
+
+    close(10)
+    
+  end subroutine readMat
+
+
+
+
+end module LinAl
